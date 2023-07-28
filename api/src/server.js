b33f3f398887edfe539a2aa9a4b0911f74ee299f
GMaiaa <gabrielmaiaa22@gmail.com>
2023-07-28 13:15:18 -0300

    Rocket Notes Backend

---
+++ api/src/server.js
@@ -0,0 +1,36 @@
+require("express-async-errors")
+
+const migrationsRun = require("./database/sqlite/migrations")
+const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
+const express = require("express");
+const routes = require("./routes")
+const cors = require("cors")
+
+migrationsRun()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+
+app.use(routes)
+
+
+app.use((error, req, res, next) => {
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
+
+const PORT = process.env.SERVER_PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT} 🟢`));
